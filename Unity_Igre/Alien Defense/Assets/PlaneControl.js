ec0194452b231103d64cf6f71b7d939088d698b3
CCasual <nejcmlakar37@gmail.com>
2016-12-14 00:49:27 +0100

    Dodan plane

---
+++ Unity_Igre/Alien Defense/Assets/PlaneControl.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+public var kontrola : GameObject;
+public var st_zadetkov = 0;
+public var smer : String;
+
+function Start () {
+
+	smer = "levo";
+
+	if(transform.position.x < 0){
+
+		transform.Rotate(0, 180, 0);
+		smer = "desno";
+	}
+}
+
+function Update(){
+
+	if(smer == "levo"){
+
+		transform.position.x = transform.position.x - 0.2;
+	}
+	else if(smer == "desno"){
+
+		transform.position.x = transform.position.x + 0.2;
+	}
+}
+
+
+function OnCollisionEnter2D(col : Collision2D){
+
+	if(col.gameObject.tag == "Meteor"){
+
+		kontrola.GetComponent.<Spawning>().zadetek();
+		Destroy(col.gameObject);
+
+		if(st_zadetkov == 3){
+
+			Destroy(gameObject);
+		}
+
+		st_zadetkov++;
+	}
+}
--- Unity_Igre/Alien Defense/Assets/Spawning.js
+++ Unity_Igre/Alien Defense/Assets/Spawning.js
@@ -1,17 +1,26 @@
 ﻿#pragma strict
 
+//Spremenljivke za Instantiate
 public var prefab : GameObject;
+public var letalo: GameObject;
 public var camera : Camera;
 
+//Spremenljivke za "dosežke igralca"
 public var life : float;
 public var score : float;
+public var poraz : boolean;
+
+//Spremenljivke za merjenje časa
+var cas = 1.0;
+var stevec = 0;
 
 function Start() {
 
 	score = 0;
 	life = 10;
+	poraz = false;
 
-	InvokeRepeating("Ustvarjanje", 1.0f, 0.5f);
+	InvokeRepeating("Ustvarjanje", 1.0f, cas);
 }
 
 function Update(){
@@ -24,8 +33,53 @@ function Update(){
 
 function Ustvarjanje() {
 
+	stevec++;
+	//Pozicija meteorjev
 	var x = Random.Range(-36.5f, 43.5f);
-	Instantiate(prefab, new Vector3(x, 28.1, 0), Quaternion.identity);
+
+	//Pozicija letal
+	var l = Random.Range(3 , 20);
+
+	//Izbira objekta
+	var y = Random.Range(0, 10);
+
+	if(stevec == 5){
+
+		if(cas > 0.7){
+			
+			cas -= 0.2;
+			stevec = 0;
+		}
+	}
+
+	if(cas > 0.7)
+	{
+		Instantiate(prefab, new Vector3(x, 28.1, 0), Quaternion.identity);
+	}
+	else{
+
+		x = Random.Range(-36.5f, 43.5f);
+		Instantiate(prefab, new Vector3(x, 28.1, 0), Quaternion.identity);
+
+		x = Random.Range(-36.5f, 43.5f);
+		Instantiate(prefab, new Vector3(x, 28.1, 0), Quaternion.identity);
+
+		if(y < 0.1){
+
+			//Levo ali Desno
+			var stran = Random.Range(0,1);
+
+			if(stran < 0.5){
+
+				Instantiate(letalo, new Vector3(-43, l, 0), Quaternion.identity);
+			}
+
+			else{
+
+				Instantiate(prefab, new Vector3(50.2, l, 0), Quaternion.identity);
+			}
+		}
+	}
 
 }
 
@@ -47,5 +101,8 @@ function unicenje(){
 
 function zadetek(){
 
-	score = score + 1;
+	if(!poraz){
+
+		score = score + 1;
+	}
 }
